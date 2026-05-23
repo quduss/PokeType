9af8a7f59ee5fb23ce8bea6873cfb363fd124302
quduss <qudshuaib@gmail.com>
2026-05-23 07:52:48 +0100

    Create an object literal of supported commands

---
+++ src/commands.ts
@@ -0,0 +1,10 @@
+export function getCommands(): Record<string, CLICommand> {
+  return {
+    exit: {
+      name: "exit",
+      description: "Exits the pokedex",
+      callback: commandExit,
+    },
+    // can add more commands here
+  };
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
+import { createInterface } from "readline";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+    console.log(`Your command was: ${commandName}`);
+    rl.prompt();
+  });
+}
+
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
