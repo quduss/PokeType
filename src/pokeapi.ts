2617624cf2161d532fcff77e683f31e01f7ca1cb
quduss <qudshuaib@gmail.com>
2026-05-27 15:24:16 +0100

    Implement fetchLocations and fetchLocation async functions of the PokeAPI class

---
+++ src/pokeapi.ts
@@ -0,0 +1,42 @@
+export class PokeAPI {
+    private static readonly baseURL = "https://pokeapi.co/api/v2";
+
+    constructor() {}
+
+    async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+
+        const url = pageURL || `${PokeAPI.baseURL}/location-area`;
+
+        try {
+            const resp = await fetch(url);
+
+            if (!resp.ok) {
+                throw new Error(`${resp.status} ${resp.statusText}`);
+            }
+
+            const locations: ShallowLocations = await resp.json();
+            return locations;
+        } catch (e) {
+            throw new Error(`Error fetching locations: ${(e as Error).message}`)
+        }
+    }
+
+    async fetchLocation(locationName: string): Promise<Location> {
+        const url = `${PokeAPI.baseURL}/location-area/${locationName}`;
+
+        try {
+            const resp = await fetch(url);
+
+            if (!resp.ok) {
+                throw new Error(`${resp.status} ${resp.statusText}`);
+            }
+            
+            const location: Location = await resp.json();
+            return location;
+        } catch (e) {
+            throw new Error(
+                `Error fetching location '${locationName}': ${(e as Error).message}`,
+            );
+        }
+    }
+}
