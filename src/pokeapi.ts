c53d27b85f35bc8d50e2718bea55cc668a181bba
quduss <qudshuaib@gmail.com>
2026-05-29 11:44:50 +0100

    Create and export type Location

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -50,3 +50,56 @@ export type ShallowLocations = {
         url: string;
     }[]
 }
+
+export type Location = {
+    encounter_method_rates: {
+        encounter_method: {
+            name: string;
+            url: string;
+        };
+        version_details: {
+            rate: number;
+            version: {
+                name: string;
+                url: string;
+            };
+        }[];
+    }[];
+    game_index: number;
+    id: number;
+    location: {
+        name: string;
+        url: string;
+    };
+    name: string;
+    names: {
+        language: {
+            name: string;
+            url: string;
+        };
+        name: string;
+    }[];
+    pokemon_encounters: {
+        pokemon: {
+            name: string;
+            url: string;
+        };
+        version_details: {
+            encounter_details: {
+                chance: number;
+                condition_values: any[];
+                max_level: number;
+                method: {
+                    name: string;
+                    url: string;
+                };
+                min_level: number;
+            }[];
+            max_chance: number;
+            version: {
+                name: string;
+                url: string;
+            };
+        }[];
+    }[];
+};
