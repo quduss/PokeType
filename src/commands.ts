bb6d75f945a1b06b5243c184530e929e3c841e35
quduss <qudshuaib@gmail.com>
2026-06-02 16:50:48 +0100

    Update list of commands with the map command

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,8 @@
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
 import type { CLICommand } from "./state.js";
+import { commandMapForward } from "./command_map.js";
+
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -14,6 +16,12 @@ export function getCommands(): Record<string, CLICommand> {
       name: "help",
       description: "Displays a help message",
       callback: commandHelp,
+    },
+
+    map: {
+      name: "map",
+      description: "Get the next page of locations",
+      callback: commandMapForward,
     },
   };
 }
