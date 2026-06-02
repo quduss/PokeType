9c894def8b0e45a66e6def89c6404baf4af6eb41
quduss <qudshuaib@gmail.com>
2026-06-02 17:18:19 +0100

    Update entrypoint to treat repl loop as an async function and await callback functions

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,6 @@
 import { State } from "./state.js";
 
-export function startREPL(state: State) {
+export async function startREPL(state: State) {
 
   state.readline.prompt();
 
@@ -23,9 +23,9 @@ export function startREPL(state: State) {
     }
 
     try {
-      cmd.callback(state);
+      await cmd.callback(state);
     } catch (e) {
-      console.log(e);
+      console.log((e as Error).message);
     }
 
     state.readline.prompt();
