4ff3f53b503e8b4ec4bf6fc6a32211c29624fb0b
quduss <qudshuaib@gmail.com>
2026-06-03 15:25:59 +0100

    Create the callback function for the mapb command

--- src/command_map.ts
+++ src/command_map.ts
@@ -9,3 +9,18 @@ export async function commandMapForward(state: State) {
         console.log(loc.name);
     }
 }
+
+export async function commandMapBack(state: State) {
+  if (!state.prevLocationsURL) {
+    throw new Error("you're on the first page");
+  }
+
+  const locations = await state.pokeAPI.fetchLocations(state.prevLocationsURL);
+
+  state.nextLocationsURL = locations.next;
+  state.prevLocationsURL = locations.previous;
+
+  for (const loc of locations.results) {
+    console.log(loc.name);
+  }
+}
