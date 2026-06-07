78a7e71c3669b20052eeb510a6231c553a689e42
quduss <qudshuaib@gmail.com>
2026-06-07 18:18:43 +0100

    Update the PokeAPI class to use the cache

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,11 +1,27 @@
+import { Cache } from "./pokecache.js";
+
+
 export class PokeAPI {
     private static readonly baseURL = "https://pokeapi.co/api/v2";
+    private cache: Cache;
 
-    constructor() {}
+
+    constructor(cacheInterval: number) {
+        this.cache = new Cache(cacheInterval);
+    }
+
+    closeCache() {
+        this.cache.stopReapLoop();
+    }
 
     async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-
+        
         const url = pageURL || `${PokeAPI.baseURL}/location-area`;
+
+        const cached = this.cache.get<ShallowLocations>(url);
+        if (cached) {
+            return cached;
+        }
 
         try {
             const resp = await fetch(url);
@@ -15,6 +31,7 @@ export class PokeAPI {
             }
 
             const locations: ShallowLocations = await resp.json();
+            this.cache.add(url, locations);
             return locations;
         } catch (e) {
             throw new Error(`Error fetching locations: ${(e as Error).message}`)
