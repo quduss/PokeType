a1dc18adb51ace4695ac059b8d93cf8cace7aad1
quduss <qudshuaib@gmail.com>
2026-06-08 16:35:21 +0100

    Pass in args to callbacks

--- src/repl.ts
+++ src/repl.ts
@@ -12,6 +12,7 @@ export async function startREPL(state: State) {
     }
 
     const commandName = words[0];
+    const args = words.slice(1);
     const cmd = state.commands[commandName];
 
     if (!cmd) {
@@ -23,7 +24,7 @@ export async function startREPL(state: State) {
     }
 
     try {
-      await cmd.callback(state);
+      await cmd.callback(state, ...args);
     } catch (e) {
       console.log((e as Error).message);
     }
