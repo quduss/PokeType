39f21019f2c99f77ab2af2cad8ad2e458e0cf35a
quduss <qudshuaib@gmail.com>
2026-06-11 14:23:58 +0100

    Update the commands registry with the pokedex command

--- src/commands.ts
+++ src/commands.ts
@@ -5,6 +5,7 @@ import { commandMapForward, commandMapBack } from "./command_map.js";
 import { commandExplore } from "./command_explore.js";
 import { commandCatch } from "./command_catch.js";
 import { commandInspect } from "./command_inspect.js";
+import { commandPokedex } from "./command_pokedex.js";
 
 
 export function getCommands(): Record<string, CLICommand> {
@@ -49,6 +50,12 @@ export function getCommands(): Record<string, CLICommand> {
       name: "inspect <pokemon_name>",
       description: "View details about a caught pokemon",
       callback: commandInspect,
+    },
+
+    pokedex: {
+      name: "pokedex",
+      description: "See all the pokemon you've caught",
+      callback: commandPokedex,
     },
   };
 }
